526996a238e5a9a5bd0bd770ae9f6cd8c7b9de09
deniel isaac <dns3886a@ens.fsi>
2022-10-26 14:43:56 +0200

    humain

---
+++ src/personnages/Humain.java
@@ -0,0 +1,44 @@
+package personnages;
+
+public class Humain {
+	private String nom;
+	private String boissonFav;
+	private int argent;
+	
+	public String getNom() {
+		return nom;
+	}
+	public int getArgent() {
+		return argent;
+	}
+	
+	public Humain(String nom, String boissonFav, int argent) {
+		super();
+		this.nom = nom;
+		this.boissonFav = boissonFav;
+		this.argent = argent;
+	}
+	public void parler(String texte) {
+		System.out.println("("+nom+")"+ " - " + texte);
+	}
+	
+	public void direBonjour() {
+		parler("Bonjour ! Je m'appelle " + nom + " et j'aime boire du " + boissonFav);
+	}
+	
+	public void boire() {
+		parler("Mmm, un bon verre de " + boissonFav + " ! GLOUPS !");
+	}
+	
+	public void acheter(String bien, int prix) {
+		if (prix > argent) {
+			parler("Je n'ai plus que " + argent + " sous en poche. Je ne peux même pas m'offrir un(e) " + bien + " à " + prix + " sous."); 
+		} else {
+			parler ("J'ai " + argent + " sous en poche. Je vais pouvoir m'offrir un(e) " + bien + " à " + prix + " sous.");
+			argent -= prix;
+		}
+	}
+	
+	
+	
+}
